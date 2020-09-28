9fd35c0ac285a408a57cf5500168bf75958521a7
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-28 19:19:21 -0300

    /study's header is now styled and functional

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -24,15 +25,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Study"/>
               Study
-          </a>
+          </Link>
 
-          <a href="" className="teach">
+          <Link to="/teach" className="teach">
             <img src={teachIcon} alt="Teach"/>
               Teach
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
