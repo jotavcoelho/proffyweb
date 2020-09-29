6f9713d8e8f9e936a3539b4e3832cfea5fc84c9a
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-28 21:28:17 -0300

    TeacherList items styled on mobile

--- src/pages/TeacherList/index.jsx
+++ src/pages/TeacherList/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList() {
@@ -25,6 +27,35 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes"/>
+            <div>
+              <strong>Diego Fernandes</strong>
+              <span>Chemistry</span>
+            </div>
+          </header>
+
+          <p>
+            Enthusiast of the best chemistry technologies
+            <br/><br/>
+            In love with exploding things in a lab and changing people's lives through exp...eriences. More than 200000 people have been through one of my explosions.
+          </p>
+
+          <footer>
+            <p>
+              Price/hour
+              <strong>$80</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Get in touch
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
