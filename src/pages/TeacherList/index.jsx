3cf739a1f5853498754809e20c20ca2c53dae1aa
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-30 16:01:09 -0300

    Added the Select fields where they should be

--- src/pages/TeacherList/index.jsx
+++ src/pages/TeacherList/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -11,8 +12,33 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="These are the available proffys.">
         <form id="search-teachers">
-          <Input name="subject" label="Subject" />
-          <Input name="weekday" label="Weekday" />
+          <Select 
+            name="subject" 
+            label="Subject" 
+            options={[
+              { value: 'Art', label: 'Art' },
+              { value: 'Biology', label: 'Biology' },
+              { value: 'Physics', label: 'Physics' },
+              { value: 'Geography', label: 'Geography' },
+              { value: 'Enlgish', label: 'Enlgish' },
+              { value: 'Spanish', label: 'Spanish' },
+              { value: 'Chemistry', label: 'Chemistry' },
+              { value: 'Philosophy', label: 'Philosophy' },
+            ]}
+          />
+          <Select 
+            name="weekday" 
+            label="Weekday" 
+            options={[
+              { value: '0', label: 'Sunday' },
+              { value: '1', label: 'Monday' },
+              { value: '2', label: 'Tuesday' },
+              { value: '3', label: 'Wednesday' },
+              { value: '4', label: 'Thursday' },
+              { value: '5', label: 'Friday' },
+              { value: '6', label: 'Saturday' },
+            ]}
+          />
           <Input name="time" label="Time" type="time" />
         </form>
       </PageHeader>
