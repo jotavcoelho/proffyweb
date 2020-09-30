ec60c8e41d4b11dbaba47b80140b9514c7a649d6
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-30 15:46:46 -0300

    Created and set Textarea and Select components

---
+++ src/components/Select/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './styles.css';
+
+function Select({ label, name, options, ...rest }) {
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>{label}</label>
+      <select type="text" id={name} {...rest}>
+        <option value="" disabled selected hidden>Select an option</option>
+        {options.map(option => {
+          return <option key={option.value} value={option.value}>{option.label}</option>
+        })}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
--- src/pages/TeacherForm/index.jsx
+++ src/pages/TeacherForm/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
@@ -20,14 +22,28 @@ function TeacherForm() {
           <legend>Your data</legend>
 
           <Input name="name" label="Full name" />
-          <Input name="avatar" label="avatar" />
+          <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Bio" />
         </fieldset>
 
         <fieldset>
           <legend>About your classes</legend>
 
-          <Input name="subject" label="Subject" />
+          <Select 
+            name="subject" 
+            label="Subject" 
+            options={[
+              { value: 'Art', label: 'Art' },
+              { value: 'Biology', label: 'Biology' },
+              { value: 'Physics', label: 'Physics' },
+              { value: 'Geography', label: 'Geography' },
+              { value: 'Enlgish', label: 'Enlgish' },
+              { value: 'Spanish', label: 'Spanish' },
+              { value: 'Chemistry', label: 'Chemistry' },
+              { value: 'Philosophy', label: 'Philosophy' },
+            ]}
+          />
           <Input name="cost" label="Cost per hour of class" />
         </fieldset>
 
