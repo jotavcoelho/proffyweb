d914bff2b09bb281e48110d47b2a84554919e8d2
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-30 22:14:41 -0300

    Clicking the whatsapp button leads to a chat with the 'teacher', and it increase the value of total connections shown on landing page

--- src/components/TeacherItem/index.jsx
+++ src/components/TeacherItem/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
 function TeacherItem({ teacherData }) {
+  
+  function handleConnect(user_id) {
+    api.post('/connections', {
+      user_id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -22,10 +31,13 @@ function TeacherItem({ teacherData }) {
           Price/hour
           <strong>${teacherData.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          href={`https://wa.me/${teacherData.whatsapp}`} 
+          target="_blank"
+          onClick={() => handleConnect(teacherData.id)} >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Get in touch
-        </button>
+        </a>
       </footer>
     </article>
   )
